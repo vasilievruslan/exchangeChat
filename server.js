a3cd930a8f2df3415981b9b166681b9d36d480b1
Ruzerio <ruzero.io@gmail.com>
2018-08-03 13:05:12 +0300

    chat

---
+++ server.js
@@ -0,0 +1,107 @@
+"use strict";
+
+process.title = 'node-chat';
+
+var webSocketsServerPort = 1337;
+var webSocketServer = require('websocket').server;
+var http = require('http');
+var fs = require('fs');
+
+/**
+ * Global variables
+ */
+
+var history = [ ];
+var clients = [ ];
+
+/**
+ * Helper function for escaping input strings
+ */
+function htmlEntities(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;')
+                      .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
+
+var colors = [ '#EF5777', '#bf209f', '#d4d6cb', '#F89C35', '#84B1ED', '#AACD6E', '#f26d5b' ];
+
+colors.sort(function(a,b) { return Math.random() > 0.5; } );
+
+/**
+ * HTTP server
+ */
+var server = http.createServer(function(request, response) {
+    
+});
+server.listen(webSocketsServerPort, function() {
+    console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
+});
+
+/**
+ * WebSocket server
+ */
+var wsServer = new webSocketServer({    
+    httpServer: server
+});
+
+wsServer.on('request', function(request) {
+    console.log((new Date()) + ' Connection from origin ' + request.origin + '.');    
+    
+    var connection = request.accept(null, request.origin); 
+    
+    var index = clients.push(connection) - 1;
+    var userName = false;
+    var userColor = false;
+
+    console.log((new Date()) + ' Connection accepted.');
+
+    
+    if (history.length > 0) {
+        connection.sendUTF(JSON.stringify( { type: 'history', data: history} ));
+    }
+
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') { 
+            if (userName === false) { 
+                
+                userName = htmlEntities(message.utf8Data);
+                
+                userColor = colors.shift();
+                connection.sendUTF(JSON.stringify({ type:'color', data: userColor }));
+                console.log((new Date()) + ' User is known as: ' + userName
+                            + ' with ' + userColor + ' color.');
+            } else { 
+                console.log((new Date()) + ' Received Message from '
+                            + userName + ': ' + message.utf8Data);
+
+                var obj = {
+                    time: (new Date()).getTime(),
+                    text: htmlEntities(message.utf8Data),
+                    author: userName,
+                    color: userColor
+                };
+                history.push(obj);
+                history = history.slice(-100);
+                
+                var json = JSON.stringify({ type:'message', data: obj });
+                for (var i=0; i < clients.length; i++) {
+                    clients[i].sendUTF(json);
+                }
+            }
+        }
+    });
+
+    connection.on('close', function(connection) {
+        if (userName !== false && userColor !== false) {
+            console.log((new Date()) + " Peer "
+                + connection.remoteAddress + " disconnected.");
+            
+            clients.splice(index, 1);
+            colors.push(userColor);
+        }
+    });
+});
+
+
+
+
